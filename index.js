07708a317c1836a9616217cbe376f18f8b9d2415
Abhishek Verma <avishekv444@gmail.com>
2021-08-12 16:50:02 +0530

    added delete button functionality

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hii');
+deleteTask();
+function deleteTask(){
+    let deleteList=document.getElementsByClassName("delete-tasks");
+    var i;
+    for(i=0;i<deleteList.length;i++){
+        deleteList[i].addEventListener("click",function(){
+            var div=this.parentElement;
+            div.style.display="none";
+        })
+    }
+}
