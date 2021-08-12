2ca1cc2323e01f4a26069bb87ec61b77eec712ca
Abhishek Verma <avishekv444@gmail.com>
2021-08-12 17:22:59 +0530

    added Add Task button functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//delete button functionality:-to delete the tasks from tasks list
 deleteTask();
 function deleteTask(){
     let deleteList=document.getElementsByClassName("delete-tasks");
@@ -8,4 +9,24 @@ function deleteTask(){
             div.style.display="none";
         })
     }
+}
+
+//ADD task button functionality:-To add the task in tasks list
+let addButton=document.getElementById("add-button");
+addButton.onclick=function(){
+    var inputValue=document.getElementById("task").value;
+    var div=document.createElement("div");
+    var li=document.createElement("li");
+    var i=document.createElement("i");
+    if(inputValue=== ""){
+        alert("You Must Write Something");
+    }else{
+        i.className="fas fa-trash delete-tasks";
+        li.innerHTML=inputValue;
+        div.append(li,i);
+        var myTasksList=document.getElementById('tasks-container');
+        myTasksList.append(div);
+        deleteTask();    
+    }
+   
 }
