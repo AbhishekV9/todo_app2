f659fd48e743106c9f441331705bf102ea13eb51
Abhishek Verma <avishekv444@gmail.com>
2021-08-13 14:05:25 +0530

    added more functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var noOftasks=0;
+
 //delete button functionality:-to delete the tasks from tasks list
 deleteTask();
 function deleteTask(){
@@ -6,9 +8,11 @@ function deleteTask(){
     for(i=0;i<deleteList.length;i++){
         deleteList[i].addEventListener("click",function(){
             var div=this.parentElement;
-            div.style.display="none";
+            noOftasks--;
+            div.remove();
         })
     }
+
 }
 
 function changeClass(div){
@@ -20,7 +24,8 @@ let addButton=document.getElementById("add-button");
 addButton.onclick=function(){
     var inputValue=document.getElementById("task").value;
     var div=document.createElement("div");
-    div.className="uncomplete"
+    div.className="uncomplete";
+    div.classList.add("tsk");
     var li=document.createElement("li");
     var i=document.createElement("i");
     if(inputValue=== ""){
@@ -31,13 +36,13 @@ addButton.onclick=function(){
         div.append(li,i);
         var myTasksList=document.getElementById('tasks-container');
         myTasksList.append(div);
+        noOftasks++;
         deleteTask(); 
-        changeClass(div);   
     }
    
 }
 
-//All button functionality
+//All button functionality:to show both uncomplete and completed tasks
 var allButton=document.getElementById('all-button');
 allButton.onclick=function(){
     var tasksList=document.getElementsByClassName("complete");
@@ -48,4 +53,60 @@ allButton.onclick=function(){
     for(let i=0;i<tasksList.length;i++){
         tasksList[i].style.display='initial';
     }
+}
+
+//Completed button functionality:to show only completed tasks
+var completeButton=document.getElementById('complete-button');
+completeButton.onclick=function(){
+    var tasksList=document.getElementsByClassName("complete");
+    for(let i=0;i<tasksList.length;i++){
+        tasksList[i].style.display='initial';
+    }
+    var tasksList=document.getElementsByClassName('uncomplete');
+    for(let i=0;i<tasksList.length;i++){
+        tasksList[i].style.display='none';
+    }
+}
+
+//uncomplete button functionality:to show only uncomplete tasks
+var uncompleteButton=document.getElementById('uncomplete-button');
+uncompleteButton.onclick=function(){
+    var tasksList=document.getElementsByClassName("complete");
+    for(let i=0;i<tasksList.length;i++){
+        tasksList[i].style.display='none';
+    }
+    var tasksList=document.getElementsByClassName('uncomplete');
+    for(let i=0;i<tasksList.length;i++){
+        tasksList[i].style.display='initial';
+    }
+}
+
+
+
+//complete all tasks button
+var completeAll=document.getElementById("complete-all");
+completeAll.onclick=function(){
+    var totalDiv=document.querySelectorAll("ul .tsk");
+
+    //console.log(totalDiv);
+    for(var i=0;i<totalDiv.length;i++){
+       if(totalDiv[i].classList.contains("uncomplete")){
+           totalDiv[i].classList.remove("uncomplete");
+           totalDiv[i].classList.add("complete"); 
+       }
+    }
+    //console.log(totalDiv);
+}
+
+//Clear All Completed Task button
+var clearAllbtn=document.getElementById('clear-complete');
+clearAllbtn.onclick=function(){
+    var tasksList=document.querySelectorAll("ul .complete");
+    // console.log(tasksList.length);
+    // console.log(tasksList);
+    for(var i=0;i<tasksList.length;i++){
+         console.log(i);
+        tasksList[i].remove();
+    }
+    console.log(tasksList);
 }
